d2053e5bc35c01a6838ac8114f67824c6b131846
Tony Bradley <antbradley91@gmail.com>
2017-02-05 16:52:21 -0500

    facebook pictures

--- client/pages/house-members.js
+++ client/pages/house-members.js
@@ -5,6 +5,12 @@ const navbar = require('../elements/navbar')
 function houseMembers(state, prev, send) {
 	const filter = state.members.searchFilters.house
 
+	FBready.then(function () {
+		FB.api('/19787529402/picture?width=480&height=480', function (res) {
+			console.log('res = ', res)
+		})
+	})
+
 	function fetchHouseMembers() {
 		send('members:fetchMembers', {chamber: 'house'})
 	}
