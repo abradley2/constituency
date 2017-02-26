ed7af9eb1c817c2dca850f49484a5cbaf086595a
Tony Bradley <antbradley91@gmail.com>
2017-02-26 16:42:16 -0500

    more work on votes

--- client/pages/member-votes.js
+++ client/pages/member-votes.js
@@ -3,6 +3,7 @@ const css = require('sheetify')
 const xhr = require('xhr')
 const i = require('icepick')
 const navbar = require('../elements/navbar')
+const profileCard = require('../elements/profile-card')
 
 const container = css`
 	:host {
@@ -45,11 +46,18 @@ const memberVotesModel = {
 
 function memberVotes(state, prev, send) {
 	const memberId = state.location.params.memberId
+	const memberInfo = state.memberProfile.memberInfo
 	const votes = state.memberVotes.votes
 
 	function init() {
 		send('memberVotes:init')
 		send('memberVotes:fetchMemberVotes', {memberId: memberId})
+		if (
+			!memberInfo ||
+			!memberInfo.member_id !== memberId
+		) {
+			send('memberProfile:fetchMemberInfo', {memberId: memberId})
+		}
 	}
 
 	if (prev && prev.location.params.memberId !== state.location.params.memberId) {
@@ -58,15 +66,24 @@ function memberVotes(state, prev, send) {
 
 	return html`<div class='center ${container}' onload=${init}>
 		${navbar()}
-		<h3>member votes</h3>
+		<div>
+		${memberInfo ?
+			profileCard({member: memberInfo}) :
+			null
+		}
+		</div>
+		<div class='tc'>
+			<h3 class='lh-title'>Recent Votes</h3>
+		</div>
 		<ul class='list pa2'>
 			${votes
 				.slice(0, state.memberVotes.showMore ? 100 : 15)
 				.map(function (vote) {
 					return html`<li class='mb5'>
 						<div>
-							<a class='link'>Full Vote</a>
-							<a class='ml-3 link'>Bill Details</a>
+							<a class='link' href='/votes/${vote.roll_call}'>Full Vote</a>
+							<span class='ml2 mr2'>|</span>
+							<a class='ml-3 link' href='/bills/${vote.bill.id}'>Bill Details</a>
 						</div>
 						<span>${vote.bill.title}</span>
 					</li>`
