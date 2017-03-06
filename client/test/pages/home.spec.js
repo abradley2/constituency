8131ceb80719217d697331590aa5bb884040702f
Tony Bradley <antbradley91@gmail.com>
2017-03-06 13:37:48 -0500

    hyperx for awesome testing

--- client/test/pages/home.spec.js
+++ client/test/pages/home.spec.js
@@ -1,11 +1,25 @@
 const test = require('tape')
 const choo = require('choo')
+const hyperx = require('hyperx')
+
+const html = hyperx(pojo)
 
 const app = choo()
 const sut = require('../../pages/home')(app)
 
 test('pages/home', function (t) {
 	const rendered = sut({}, {}, Function.prototype)
+	const parsed = html([rendered.outerHTML.toString()])
+
 	t.ok(rendered)
+	t.ok(parsed)
 	t.end()
 })
+
+function pojo(tag, attributes, children) {
+	return {
+		tag: tag,
+		attributes: attributes,
+		children: children
+	}
+}
